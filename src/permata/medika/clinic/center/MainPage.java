e192fd35b40580db2c1ec108c93602ce0d71957b
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-11-06 07:25:59 +0700

    add login view

--- src/permata/medika/clinic/center/MainPage.java
+++ src/permata/medika/clinic/center/MainPage.java
@@ -3,18 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package permata.medika.clinic.center;
-import java.awt.Color;
 
 /**
  *
  * @author nico
  */
-public class LoginForm extends javax.swing.JFrame {
+public class MainPage extends javax.swing.JFrame {
 
     /**
-     * Creates new form LoginForm
+     * Creates new form MainPage
      */
-    public LoginForm() {
+    public MainPage() {
         initComponents();
     }
 
@@ -27,35 +26,10 @@ public class LoginForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btn_login = new javax.swing.JLabel();
-        tf_password = new javax.swing.JTextField();
-        tf_username = new javax.swing.JTextField();
-        bg_login = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
-        getContentPane().add(btn_login);
-        btn_login.setBounds(230, 520, 200, 50);
 
-        tf_password.setBackground(new Color(0,0,0,0));
-        tf_password.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
-        tf_password.setBorder(null);
-        getContentPane().add(tf_password);
-        tf_password.setBounds(250, 450, 330, 50);
-
-        tf_username.setBackground(new Color(0,0,0,0));
-        tf_username.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
-        tf_username.setBorder(null);
-        getContentPane().add(tf_username);
-        tf_username.setBounds(250, 360, 330, 50);
-
-        bg_login.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Login.png"))); // NOI18N
-        getContentPane().add(bg_login);
-        bg_login.setBounds(0, 0, 1512, 856);
-        bg_login.getAccessibleContext().setAccessibleName("");
-
-        setSize(new java.awt.Dimension(1512, 884));
-        setLocationRelativeTo(null);
+        setBounds(0, 0, 1512, 884);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -75,28 +49,24 @@ public class LoginForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LoginForm().setVisible(true);
+                new MainPage().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel bg_login;
-    private javax.swing.JLabel btn_login;
-    private javax.swing.JTextField tf_password;
-    private javax.swing.JTextField tf_username;
     // End of variables declaration//GEN-END:variables
 }
